4391e332868622a6df5c5a85a8a3d70e475c536f
king_ok_er <zolotojznak@gmail.com>
2022-08-10 12:11:34 +0500

    static settings added

--- root/settings.py
+++ root/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -111,18 +111,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / "astramedClinic/static",
+    BASE_DIR/"astramedClinic/static",
 ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/"media/" #Для хостинга
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #Для виртуалки
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
